61dfcfd166a6262352e8db912190b06ef30d36c9
goju2k <goju2k@naver.com>
2021-09-30 22:38:05 +0900

    common layout setting

--- components/layout/PageContainer.vue
+++ components/layout/PageContainer.vue
@@ -1,6 +1,18 @@
 <template>
+  <!-- root -->
   <div class="page-container">
-    <slot />
+    <!-- header -->
+    <div class="page-container-header">
+      <slot name="header" />
+    </div>
+    <!-- contents -->
+    <div class="page-container-contents">
+      <slot />
+    </div>
+    <!-- footer -->
+    <div class="page-container-footer">
+      <slot name="footer" />
+    </div>
   </div>
 </template>
 
@@ -11,17 +23,47 @@ export default {
 </script>
 
 <style>
+
 *{
   box-sizing: border-box;
 }
+
 body{
   margin:0px;
+  font-size:16px;
 }
 
 .page-container{
-    width:calc(100vw - 20px);height:calc(100vh - 20px);
-    border:1px solid red;
-    margin:10px;
-    padding:20px;
+  width:100vw;height:100vh;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  background:white;
 }
+
+.page-container-header{
+  width:100%;height:40px;background:orange;opacity: 0.5;
+}
+
+.page-container-footer{
+  width:100%;height:40px;background:blue;opacity: 0.5;
+}
+
+.page-container-contents{
+  width:100%;
+  height:calc(100% - 80px);
+  overflow-y:auto;
+  background:rgb(243, 243, 243);
+}
+
+@media screen and (min-width: 900px) {
+  body{
+    font-size:16px;
+  }
+  .page-container-contents{
+    width:900px;
+    overflow:auto;
+  }
+}
+
 </style>
--- pages/gate/welcome.vue
+++ pages/gate/welcome.vue
@@ -1,6 +1,11 @@
 <template>
   <page-container>
-    <div>welcome!!!</div>
+    <div>환영합니다. welcome!!!</div>
+    <template v-for="i in 200">
+      <div :key="i">
+        {{ i+'번째 라인!!!' }}
+      </div>
+    </template>
     <nuxt-link to="/find/find-main">
       main gogo
     </nuxt-link>
